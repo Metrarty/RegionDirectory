5ecbeb1673392680f0b26cd27f35e7dc0d859f64
Artem Oshkoder <77104270+Metrarty@users.noreply.github.com>
2021-05-26 14:09:45 +0300

    Add files via upload

---
+++ src/main/java/com/example/RegionDirectory/RegionDirectoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.RegionDirectory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegionDirectoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegionDirectoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RegionDirectory/RegionDirectoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RegionDirectory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegionDirectoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
